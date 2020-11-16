b9e1b4b2391dc0d2f496caf07dd2e7d267f63bc1
wlytle <wlytle89@gmail.com>
2020-11-15 19:28:44 -0700

    commit tree e25c895b2b34d3fa6e919331e281c067551a2b62

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ let game = document.getElementById("game");
 const dodgerWidth = dodger.offsetWidth;
 const gameWidth = game.offsetWidth;
 
-moveDodgerLeft = () => {
-  let leftNumbers = dodger.style.left.replace("px", "");
-  let left = parseInt(leftNumbers, 10);
-  if (left > 0) {
-    dodger.style.left = `${left - 1}px`;
-  }
-};
-
-moveDodgerRight = () => {
+function moveDodgerRight() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
   let right = parseInt(leftNumbers, 10) + dodgerWidth;
   if (right < gameWidth) {
     dodger.style.left = `${left + 1}px`;
   }
-};
+}
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
 
 document.addEventListener("keydown", function (e) {
   switch (e.key) {
